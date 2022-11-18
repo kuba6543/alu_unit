2b93f2d3fbd6900cea92e488e8350455a32b6fa3
Jakub Bartuszek <01169002@pw.edu.pl>
2022-11-18 11:50:50 +0100

    Further development of a project, implementation of sequentional block

--- README.md
+++ README.md
@@ -9,12 +9,17 @@ Projekt zawiera zmienne wejściowe:
 
 Projekt zawiera zmienne wyjściowe:
 1. o_result (tyle samo bitów co wejścia i_argA i i_argB) - Dane wyjściowe modułu.
-2. o_status (3 bity) - wyjście informujące o ewentualnych błędach lub innych informacjach:
-    * 000 - OK - brak błędów lub innych informacji.
-    * 001 - ERROR - błąd operacji, wartość o_result jest nieokreślona.
-    * 010 - ONES - wszystkie bity wyjścia o_result to 1.
-    * 011 - OVERFLOW - nastąpiło przepełnienie wyjścia, przez co nie można zapisać prawidłowej wartości wyjścia.
-    * 100 - ODD - w wyniku jest nieparzysta ilość jedynek.
+2. o_status (4 bity) - wyjście informujące o ewentualnych błędach lub innych informacjach:
+    * Pierwszy bit - ERROR - błąd operacji, wartość o_result jest nieokreślona.
+    * Drugi bit - ONES - wszystkie bity wyjścia o_result to 1.
+    * Trzeci bit - OVERFLOW - nastąpiło przepełnienie wyjścia, przez co nie można zapisać prawidłowej wartości wyjścia.
+    * Czwarty bit - ODD - w wyniku jest nieparzysta ilość jedynek.
+
+Dodatkowo, projekt zawiera zmienne wewnętrzne, potrzebne do prawidłowego działania układu sekwencyjnego:
+
+	* q_result - wyjście zawierające wynik układu kombinacyjnego.
+	* q_status - wyjscie zawierające informacje o ewentualnych błędach lub innych informacjach.
+
 
 ---
 
--- WORK/exe_unit_w15.sv
+++ WORK/exe_unit_w15.sv
@@ -1,111 +1,63 @@
-module exe_unit_w15_case0(i_oper, i_argA, i_argB, i_clk, i_rsn, o_result, o_status);
-    
-    parameter ARG_BYTES = 4;  
-
-    input unsigned logic [1:0]i_oper;
-    input logic [ARG_BYTES-1:0]i_argA;
-    input logic [ARG_BYTES-1:0]i_argB;
-    input logic i_clk;
-    input logic i_rsn;
-
-    output logic [ARG_BYTES-1:0]o_result;
-    output unsigned logic [3:0] o_status;
-    begin
-
-    end
-endmodule
-
-module exe_unit_w15_case1(i_oper, i_argA, i_argB, i_clk, i_rsn, o_result, o_status);
-    
-    parameter ARG_BYTES = 4;  
-
-    input unsigned logic [1:0]i_oper;
-    input logic [ARG_BYTES-1:0]i_argA;
-    input logic [ARG_BYTES-1:0]i_argB;
-    input logic i_clk;
-    input logic i_rsn;
-
-    output logic [ARG_BYTES-1:0]o_result;
-    output unsigned logic [3:0] o_status;
-    begin
-
-    end
-endmodule
-
-module exe_unit_w15_case2(i_oper, i_argA, i_argB, i_clk, i_rsn, o_result, o_status);
-    
-    parameter ARG_BYTES = 4;  
-
-    input unsigned logic [1:0]i_oper;
-    input logic [ARG_BYTES-1:0]i_argA;
-    input logic [ARG_BYTES-1:0]i_argB;
-    input logic i_clk;
-    input logic i_rsn;
-
-    output logic [ARG_BYTES-1:0]o_result;
-    output unsigned logic [3:0] o_status;
-    begin
-
-    end
-endmodule
-
-module exe_unit_w15_case3(i_oper, i_argA, i_argB, i_clk, i_rsn, o_result, o_status);
-    
-    parameter ARG_BYTES = 4;  
-
-    input unsigned logic [1:0]i_oper;
-    input logic [ARG_BYTES-1:0]i_argA;
-    input logic [ARG_BYTES-1:0]i_argB;
-    input logic i_clk;
-    input logic i_rsn;
-
-    output logic [ARG_BYTES-1:0]o_result;
-    output unsigned logic [3:0] o_status;
-    begin
-
-    end
-endmodule
-
 module exe_unit_w15(i_oper, i_argA, i_argB, i_clk, i_rsn, o_result, o_status);
 
     parameter ARG_BYTES = 4;  
     input unsigned logic [1:0]i_oper;
     input logic [ARG_BYTES-1:0]i_argA;
-    input logic [ARG_BYTES-1:0]i_argB;
+    input logic [ARG_BYTES-1:0]i_argB
+    ;
     input logic i_clk;
     input logic i_rsn;
+
     output logic [ARG_BYTES-1:0] o_result;
     output unsigned logic [3:0] o_status;
+
+    logic [ARG_BYTES-1:0] q_result;
+    logic [3:0] q_status;
 
     always_comb
     begin
     case (i_oper)
         0 :
         begin
-            exe_unit_w15_case0 #(.ARG_BYTES(ARG_BYTES)) case0
-            (.i_oper(i_oper), .i_argA(i_argA), .i_argB(i_argB), .i_clk(i_clk), .i_rsn(i_rsn), .o_result(o_result), .o_status(o_status));
+
         end
         1:
         begin
-            exe_unit_w15_case1 #(.ARG_BYTES(ARG_BYTES)) case1
-            (.i_oper(i_oper), .i_argA(i_argA), .i_argB(i_argB), .i_clk(i_clk), .i_rsn(i_rsn), .o_result(o_result), .o_status(o_status));
+
         end
         2:
         begin
-            exe_unit_w15_case2 #(.ARG_BYTES(ARG_BYTES)) case2
-            (.i_oper(i_oper), .i_argA(i_argA), .i_argB(i_argB), .i_clk(i_clk), .i_rsn(i_rsn), .o_result(o_result), .o_status(o_status));    
-        end
+
+	end
         3:
         begin
-            exe_unit_w15_case3 #(.ARG_BYTES(ARG_BYTES)) case3
-            (.i_oper(i_oper), .i_argA(i_argA), .i_argB(i_argB), .i_clk(i_clk), .i_rsn(i_rsn), .o_result(o_result), .o_status(o_status));
+
         end
         default:
         begin
-            o_result ='0;
-            o_status[3] = 1;
-        end
-    endcase
+            q_result ='0;
+            q_status[3] = 1;
     end
+    endcase
+
+    if (q_result ^ == 1) q_status[0]=1;
+    if (q_result && == 1) q_status[2]=1;
+    end
+
+    always_ff (posedge i_clk)
+    begin
+	    if (i_rsn == 0) 
+	    begin
+		o_result <= '0;
+		o_status <= '0;
+	    end
+
+	    else
+	    begin
+		    o_result <= q_result;
+		    o_status <= q_status;
+	    end
+    end
+
 
 endmodule
